01ca6174141f59cab8ca6d4d9792f7166dc2e3e2
Dima2908508 <99015218+Dima2908508@users.noreply.github.com>
2022-08-18 17:16:09 +0300

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,15 @@
-// Находим форму в DOM
 let popup = document.querySelector(".popup"); // Воспользуйтесь методом querySelector()
-// Находим поля формы в DOM
 let profileEditButton = document.querySelector('.profile__edit-button');     // кнопка редактирования профиля
 let nameOutput = document.querySelector('.profile__name');
 let aboutOutput = document.querySelector('.profile__about');
-
 let closePopupEditButton = document.querySelector('.popup__close');          // кнопка закрытия редактирования профиля
 let closePopupAddButton = document.querySelector('.popup__close-add');       // кнпока закрытия добавления новой карточки
-
 let newCardElements = document.querySelector('.elements');                     // пуской блок, место куда всталю карточку
-
 // Форма редактирования профилья ----------------------------------------------------
 let popupForm = document.querySelector('.popup__form');
 let nameInput = popupForm.querySelector('.popup__form-input_type_name');
 let aboutInput = popupForm.querySelector('.popup__form-input_type_about');
-
-
-
 let popupEditProfile = document.querySelector('.popup_edit-profile');
-
 // Форма добавления новой карточки-----------------------------
 let popupNewCardForm = document.querySelector('.popup__form_new-card');
 let newCardTitle = popupNewCardForm.querySelector('.popup__form-input_type_title');
@@ -26,8 +17,6 @@ let newCardLink = popupNewCardForm.querySelector('.popup__form-input_type_link')
 
 let popupAddCard = document.querySelector('.popup_new-card');
 let profileNewCardButton = document.querySelector('.profile__add-button');
-
-
 
 // ф-ия сбора карточки из темплейт
 
@@ -59,11 +48,6 @@ function createCard(cardElement) {
 }
 
 
-// function newCard(popupAddCard) {
-// popup.classList.add('popup_opened')
-// };
-
-
 function openPopup(popup) {
    popup.classList.add('popup_opened');
 }
@@ -71,7 +55,6 @@ function openPopup(popup) {
 function closePopup(popup) {
    popup.classList.remove('popup_opened');
 }
-
 
 // Блок редактирования профиля -----------------------------------------------
 function formSubmitHandler(evt) {
