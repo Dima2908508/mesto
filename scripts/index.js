c458b7fb855222026272e3e7482f686c50c2550f
Dima2908508 <99015218+Dima2908508@users.noreply.github.com>
2022-08-26 15:40:55 +0300

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -5,31 +5,33 @@ let aboutOutput = document.querySelector('.profile__about');
 let closePopupEditButton = document.querySelector('.popup__close');          // кнопка закрытия редактирования профиля
 let closePopupAddButton = document.querySelector('.popup__close-add');       // кнпока закрытия добавления новой карточки
 
-let newCardElements = document.querySelector('.elements');                     // пуской блок, место куда всталю карточку
+let newCardElements = document.querySelector('.elements');                   // пуской блок, место куда всталю карточку
 // Форма редактирования профилья  ----------------------------------------------------
 let popupForm = document.querySelector('.popup__form');
 let nameInput = popupForm.querySelector('.popup__form-input_type_name');
 let aboutInput = popupForm.querySelector('.popup__form-input_type_about');
 let popupEditProfile = document.querySelector('.popup_edit-profile');
-// Форма добавления новой карточки  -----------------------------
+// Форма добавления новой карточки  --------------------------------------------------
 let popupNewCardForm = document.querySelector('.popup__form_new-card');
 let newCardTitle = popupNewCardForm.querySelector('.popup__form-input_type_title');
 let newCardLink = popupNewCardForm.querySelector('.popup__form-input_type_link');
 
 let popupAddCard = document.querySelector('.popup_new-card');
 let profileNewCardButton = document.querySelector('.profile__add-button');
-// Форма открытия полной карточки по клику -----------------------------
-let openFullImage = document.querySelector('.popup__image-full');      //выбираем нашу картинку
-let popoupDescription = document.querySelector('.popup__description');  //подпись к карточки
-let openPopoupFullImage = document.querySelector('.popup_full-card'); //открытие окна на полной карточке 
-let closePopupFullImage = document.querySelector('.popup__close-full'); //закрытие окна полной  картинки
+// Форма открытия полной карточки по клику ------------------------------------------
+let imagePopup = document.querySelector('.popup__image-full');                       //выбираем нашу картинку
+let imagePopupDescription = document.querySelector('.popup__description');           //подпись к карточки
+let popupZoomImage = document.querySelector('.popup_full-card');                     //открытие окна на полной карточке 
+let closePopupFullImage = document.querySelector('.popup__close-full');              //закрытие окна полной  картинки
+
+let cardTemplate = document.querySelector('#element-template').content;
 
 // ф-ия сбора карточки из темплейт
 
 function createCardTemplate(name, link) {
-   let cardTemplate = document.querySelector('#element-template').content;
    let cardElement = cardTemplate.querySelector('.element').cloneNode(true);
    cardElement.querySelector('.element__photo').src = link;
+   cardElement.querySelector('.element__photo').alt = name;
    cardElement.querySelector('.element__name').textContent = name;
 
    cardElement.querySelector('.element__like').addEventListener('click', function(e) {       // переключатель лайка, как в тренажере
@@ -41,21 +43,21 @@ function createCardTemplate(name, link) {
    });
 
    cardElement.querySelector('.element__photo').addEventListener('click', function() {               //передаем данные в параметры ф-ии
-      openFullImage.src = link;
-      popoupDescription.textContent = name;
-      openPopup(openPopoupFullImage);
+      imagePopup.src = link;
+      imagePopup.alt = link;
+      imagePopupDescription.textContent = name;
+      openPopup(popupZoomImage);
    })
 
    return cardElement;
 };
 
 
-// ф-ия добавления 6 карточек из массива на страницу (созданный пустой блок для вставки)
 initialCards.forEach(function(element) {
-   newCardElements.append(createCardTemplate(element.name, element.link)); 
- });
+   let cardElement = createCardTemplate(element.name, element.link);
+   createCard(cardElement);
+})
 
-// ф-ия добовдения карточки на старницу DOM, перед массивом
 function createCard(cardElement) {
    newCardElements.prepend(cardElement);
 }
@@ -69,7 +71,7 @@ function closePopup(popup) {
 }
 
 // Блок редактирования профиля -----------------------------------------------
-function formSubmitHandler(evt) {
+function submitEditProfileForm(evt) {
    evt.preventDefault();
    nameOutput.textContent = nameInput.value;
    aboutOutput.textContent = aboutInput.value;
@@ -93,7 +95,7 @@ function formSubmitHandlerNewCard(evt) {
 }
 
 popupNewCardForm.addEventListener('submit', formSubmitHandlerNewCard);
-popupForm.addEventListener('submit', formSubmitHandler);
+popupForm.addEventListener('submit', submitEditProfileForm);
 
 closePopupEditButton.addEventListener('click', function() {
    closePopup(popupEditProfile);
@@ -104,14 +106,13 @@ closePopupAddButton.addEventListener('click', function() {
 });
 
 profileNewCardButton.addEventListener('click', function() {
-   // console.log('ЭТО НАЭЖАТИЕ НА ДОБАВЛЕНИЕ КАРТОЧИ');
    openPopup(popupAddCard);
 });
 
-openFullImage.addEventListener('click', function() {
-   openPopup(openPopoupFullImage);
+imagePopup.addEventListener('click', function() {
+   openPopup(popupZoomImage);
 })
 
 closePopupFullImage.addEventListener('click', function() {
-   closePopup(openPopoupFullImage);
+   closePopup(popupZoomImage);
 })
